c911fda9e8d73011a83f72553c57605bb5766374
Victor Batalha <madivcb@gmail.com>
2021-02-22 21:17:03 -0300

    chore: setup i18n

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
-const App = () => (
-  <>
-    <StatusBar barStyle="dark-content" />
-  </>
-);
+import '@i18n/index';
+
+const App = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+
+      <Text>{t('home')}</Text>
+    </>
+  );
+};
 
 export default App;
