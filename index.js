71d4103a3ce326f93b506291bf42cab1129e5408
Victor Batalha <madivcb@gmail.com>
2021-02-24 00:48:21 -0300

    chore: import react-native-get-random-values

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /**
- * @format
+ * This allows the usage of the function crypto.getRandomValues,
+ * which is used to generate UUIDs.
  */
-
-import {AppRegistry} from 'react-native';
+import 'react-native-get-random-values';
+import { AppRegistry } from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
